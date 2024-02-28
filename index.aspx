59a45a12023a1fb633283a0275e948423a083dd0
Arpit Nuwal <45119425+arpitnuwal@users.noreply.github.com>
2024-02-28 09:39:32 +0530

    Add files via upload
    Create img name Folder

---
+++ index.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Button ID="Button1" runat="server" Text="Upload"  OnClick="Button1_Click"/>
+
+
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+         string  filename = "new.jpg";
+            VaryQualityLevel(FileUpload1.PostedFile.InputStream, filename);
+           
+        }
+    }
+
+    private void VaryQualityLevel(Stream stream, string fname)
+    {
+
+
+
+        //  size 
+        System.Drawing.Image photo = new Bitmap(stream);
+       
+
+       
+        // without size
+          Bitmap bmp1 = new Bitmap(stream);
+        ImageCodecInfo jgpEncoder = GetEncoder(ImageFormat.Jpeg);
+        System.Drawing.Imaging.Encoder myEncoder = System.Drawing.Imaging.Encoder.Quality;
+        EncoderParameters myEncoderParameters = new EncoderParameters(1);
+        EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 30L);
+        myEncoderParameters.Param[0] = myEncoderParameter;
+        bmp1.Save(Server.MapPath("~/img/" + fname), jgpEncoder, myEncoderParameters);
+        bmp1.Dispose();
+
+    }
+    private ImageCodecInfo GetEncoder(ImageFormat format)
+    {
+        ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+        foreach (ImageCodecInfo codec in codecs)
+        {
+            if (codec.FormatID == format.Guid)
+            {
+                return codec;
+            }
+        }
+        return null;
+
+    }
+}
